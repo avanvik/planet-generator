599b75935890fe0dfd8a0913b3b47ee0face3b88
Alexander Vanvik <alexander.vanvik@gmail.com>
2019-09-01 15:33:43 +0200

    Add world generation scripts

--- Assets/Scripts/TemporaryWorldGenerator.cs
+++ Assets/Scripts/TemporaryWorldGenerator.cs
@@ -1,19 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class TemporaryWorldGenerator : MonoBehaviour
 {    
 	public Material material;
 	public int resolution;
+	
+	public ShapeSettings shapeSettings;
 
 	private void Awake()
 	{
-		var terrainConfig = new WorldTerrainConfig() {
-			Material = material,
-			Resolution = resolution,
-			ShapeSettings = new ShapeSettings()
-		};
-		var terrain = WorldTerrainGenerator.GenerateWorldTerrain(terrainConfig);
+		Generate();
+	}
+
+	private void OnValidate() {
+		UnityEditor.EditorApplication.delayCall += () => Generate();
+	}
+
+	public void Generate() {
+		if (this != null) // No idea why
+		{
+			var oldChild = transform.childCount > 0 ? transform.GetChild(0) : null;
+			if (oldChild != null) {
+				DestroyImmediate(oldChild.gameObject);
+			}
+		
+			var terrainConfig = new WorldTerrainConfig() {
+				Material = material,
+				Resolution = resolution,
+				ShapeSettings = shapeSettings
+			};
+
+			var world = WorldTerrainGenerator.GenerateWorldTerrain(terrainConfig);
+			world.transform.parent = transform;
+			world.transform.position = transform.position;
+		}
 	}
 }
